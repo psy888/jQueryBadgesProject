bc25dab4fa4c2428907e3acfd14a1f979a2c9169
Pavel <lpsyl888l@yandex.ru>
2016-12-09 15:50:10 +0200

    First Chapter with mime type error

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,15 @@
 $(function() {
-
+    // https://www.codeschool.com/users/1339588.json
   // your code will go here
-
+    $.ajax({
+        url: 'https://www.codeschool.com/users/1339588.json',
+        contentType: 'application/json',
+        dataType: "jsonp",
+        success: function(response){
+            $.each(response.courses.completed, function(i,val){
+                $('#badges').append('<div class="course"><h3>'+ val.title + '</h3><img src="' + val.badge + '"><a href="'+ val.url + '" target="_blank" class="btn btn-primary">See Course</a></div>');
+            });
+        }
+    });
+    
 });
